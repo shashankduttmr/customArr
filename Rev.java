f3c16997b9abe1f6d95615339fc0bb80ffd7f1f6
Shashankdutt <shashankduttm@gmail.com>
2024-03-04 00:15:40 +0530

    change

--- Rev.java
+++ Rev.java
@@ -14,6 +14,20 @@ class Rev{
         return true;
     }
 
+    Boolean IntChecker(int x){
+        int rev=0;
+        int min = x;
+
+        while (min != 0) {
+            int id = min % 10;
+            rev = rev*10+id;
+
+            min = min / 10;
+        }
+        return rev == x;
+
+    }
+
     public static void main(String[] args){
         Rev r = new Rev();
 
